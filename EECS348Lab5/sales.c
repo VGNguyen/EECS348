163f5bc7bfe172d13f9deae50a9e337be8dd4497
v212n917 <v212n917@ku.home.edu>
2026-03-04 19:24:11 -0600

    made more efficient printing and added min, max, and average

--- EECS348Lab5/sales.c
+++ EECS348Lab5/sales.c
@@ -10,28 +10,45 @@ int main(){
         return 1;
     }
     int line = 0;
-    char sales[12];
+    char sales[100];
     float store[12];
 
-    while(!feof(fptr) && !ferror(fptr)){
-        if (fgets(sales[line],12,fptr) !=NULL){
-            store[line] = (float)sales[line];
-            line++;
-        }
+    while(line<12 && fscanf(fptr, "%f",&store[line])==1){
+        line++;
+    }
+    const char *months[12];
+    months[0] = "January";
+    months[1] = "February";
+    months[2] = "March";
+    months[3] = "April";
+    months[4] = "May";
+    months[5] = "June";
+    months[6] = "July";
+    months[7] = "August";
+    months[8] = "September";
+    months[9] = "October";
+    months[10] = "November";
+    months[11] = "December";
+    for(int i = 0; i<12; i++) {
+        printf("%s %.2f \n", months[i], store[i]);
     }
 
-    printf("January %f",store[0]);
-    printf("Feburary %f",store[1]);
-    printf("March %f",store[2]);
-    printf("April %f",store[3]);
-    printf("May %f",store[4]);
-    printf("June %f",store[5]);
-    printf("July %f",store[6]);
-    printf("August %f",store[7]);
-    printf("September %f",store[8]);
-    printf("October %f",store[9]);
-    printf("November %f",store[10]);
-    printf("December %f",store[11]); 
+    float max = store[0];
+    float min = store[0];
+    float sum= 0;
+    for(int i =0; i<12;i++){
+        if(store[i]<min){
+            min= store[i];
+        }
+        if(store[i]>max){
+            max= store[i];
+        }
+        sum+=store[i];
+    }
+    sum = sum/12;
+    printf("Maximum Sales %.2f \n", max);
+    printf("Minimum Sales %.2f \n", min);
+    printf("Average Sales %.2f \n", sum);
 
     fclose(fptr); 
     return 0;
